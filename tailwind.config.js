458fccd2697c43fa56424e24576b1a921c1e781d
sdcvr4h2vw-spec <sdcvr4h2vw@privaterelay.appleid.com>
2025-12-14 12:29:25 +0000

    Fix Tailwind safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,20 @@
 export default {
   content: [
     "./index.html",
-    "./**/*.{ts,tsx}"
+    "./index.tsx",
+    "./App.tsx",
+    "./components/**/*.{ts,tsx}",
+    "./context/**/*.{ts,tsx}",
+    "./utils/**/*.{ts,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+safelist: [
+  'bg-red-500',
+  'bg-blue-500',
+  'text-white',
+  'text-black',
+]
