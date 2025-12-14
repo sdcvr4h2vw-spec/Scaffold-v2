ae8313cbac7e5287140fc4f41c612cfb6b3695cd
sdcvr4h2vw-spec <sdcvr4h2vw@privaterelay.appleid.com>
2025-12-14 13:06:57 +0000

    tailwind.config.js fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
+/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{ts,tsx}"
+    "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
